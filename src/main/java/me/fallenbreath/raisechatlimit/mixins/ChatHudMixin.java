cc3a14df0860c1afc5ec21991aec31cbfccbf07e
Fallen_Breath <yqx791125833@gmail.com>
2021-04-16 17:48:10 +0800

    Use linked list to store message since there might be a lot of messages

--- src/main/java/me/fallenbreath/raisechatlimit/mixins/ChatHudMixin.java
+++ src/main/java/me/fallenbreath/raisechatlimit/mixins/ChatHudMixin.java
@@ -1,16 +1,27 @@
 package me.fallenbreath.raisechatlimit.mixins;
 
+import com.google.common.collect.Lists;
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.ChatHudLine;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import java.util.List;
 
 @Mixin(ChatHud.class)
 public abstract class ChatHudMixin
 {
+	@Mutable
+	@Shadow @Final private List<ChatHudLine> messages;
+
+	@Mutable
+	@Shadow @Final private List<ChatHudLine> visibleMessages;
+
 	@ModifyConstant(
 			method = "addMessage(Lnet/minecraft/text/Text;IIZ)V",
 			constant = @Constant(intValue = 100),
@@ -25,7 +36,8 @@ public abstract class ChatHudMixin
 			method = "render",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/client/gui/hud/ChatHud;fill(IIIII)V"
+					target = "Lnet/minecraft/client/gui/hud/ChatHud;fill(IIIII)V",
+					ordinal = 1
 			)
 	)
 	private void makeSureTheScrollBarIsVisible(Args args)
@@ -46,5 +58,12 @@ public abstract class ChatHudMixin
 			args.set(1, y1);
 			args.set(3, y2);
 		}
+	}
+
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void replaceMessageContainerWithLinkedList(CallbackInfo ci)
+	{
+		this.messages = Lists.newLinkedList(this.messages);
+		this.visibleMessages = Lists.newLinkedList(this.visibleMessages);
 	}
 }
